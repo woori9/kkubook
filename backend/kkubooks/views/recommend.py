f75295a4a334903293b6802580fa5715ed11281b
shl128 <shl128@naver.com>
2022-04-06 22:10:55 +0900

    fix: 🐛 [back] 3권 미만 경우 설문기반 결과 1권 나오는 버그 해결

--- backend/kkubooks/views/recommend.py
+++ backend/kkubooks/views/recommend.py
@@ -29,7 +29,6 @@ User = get_user_model()
 @api_view(['GET'])
 def recomm_mf(request):
     if request.method == 'GET':
-        #user = get_object_or_404(User, pk=15)
         user = get_request_user(request)
 
         if not user:
@@ -75,8 +74,7 @@ def recomm_mf(request):
             if survey.category==0:  
                 recomm_category = Category.objects.all().filter(q).order_by('?')
             else:
-                recomm_category = Category.objects.filter(survey_category=survey.category).filter(Q(book__description__contains=interest_list[0]) | Q(book__description__contains=interest_list[1]) | Q(book__description__contains=interest_list[2]) | Q(book__description__contains=interest_list[3]) | Q(book__description__contains=interest_list[4])).filter(q).order_by('?')
-            
+                recomm_category = Category.objects.filter(survey_category=survey.category).filter(Q(book__description__contains=interest_list[0]) | Q(book__description__contains=interest_list[1]) | Q(book__description__contains=interest_list[2]) | Q(book__description__contains=interest_list[3]) | Q(book__description__contains=interest_list[4])).filter(q).order_by('?')          
 
             # 내 서재에 있는 책 제외
             bookshelf = Bookshelf.objects.filter(user_id=user.pk)
@@ -86,11 +84,9 @@ def recomm_mf(request):
             for book in recomm_category:
                 if book not in my_book:
                     recomm_booklist.append(Book.objects.get(pk=book.book_id))
-                    serializer = BookSearchSerializer(recomm_booklist, many=True)
-                    return Response(serializer.data)
-
                 if len(recomm_booklist) == 10:
                     break
+                serializer = BookSearchSerializer(recomm_booklist, many=True)
 
             return Response(serializer.data)
 
@@ -151,7 +147,6 @@ def recomm_feeling(request):
     **feeling
         슬퍼요(0), 떠나고 싶어요(1), 행복해요(2), 무기력해요(3), 심심해요(4), 고민이 있어요(5),
     '''   
-    # user = get_object_or_404(User, pk=4)
     user = get_request_user(request)
     if not user:
         return Response(status=HTTP_401_UNAUTHORIZED)
@@ -202,7 +197,6 @@ def recomm_best(request):
     '''
     GET: 서재에서 완독률이 높은 책 추천
     '''
-    # user = get_object_or_404(User, pk=1)
     user = get_request_user(request)
     if not user:
         return Response(status=HTTP_401_UNAUTHORIZED)
